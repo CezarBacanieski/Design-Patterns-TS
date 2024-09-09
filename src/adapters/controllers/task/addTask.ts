c198e9461aa1f3b182941b753ce3bb158b986dd4
CezarDev <bacanieskidev@gmail.com>
2024-09-09 18:57:11 -0300

    feat: creating a decorator

--- src/adapters/controllers/task/addTask.ts
+++ src/adapters/controllers/task/addTask.ts
@@ -10,6 +10,7 @@ import { Request, Response } from 'express';
 import { Controller } from '../../interfaces/controller';
 import { DateValidator } from '../../interfaces/dateValidator';
 import { AddTask } from '../../../usecases/addTask';
+import { serverError } from '../../presentations/api/httpResponses/httpResponses';
 
 export class AddTaskController implements Controller {
   constructor(
@@ -17,22 +18,26 @@ export class AddTaskController implements Controller {
     private readonly dateValidator: DateValidator
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['title', 'description', 'date'];
+    try {
+      const requiredFields = ['title', 'description', 'date'];
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
+      const { title, description, date } = httpRequest.body;
+
+      const isValid = this.dateValidator.isValid(date);
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError('date'));
+      }
+
+      const task = await this.AddTask.add({ title, description, date });
+      return created(task);
+    } catch (err: any) {
+      return serverError(err);
     }
-    const { title, description, date } = httpRequest.body;
-
-    const isValid = this.dateValidator.isValid(date);
-
-    if (!isValid) {
-      return badRequest(new InvalidParamError('date'));
-    }
-
-    const task = await this.AddTask.add({ title, description, date });
-    return created(task);
   }
 }
